1fc03bbeb5399f41296bd07f0aa03ed5170cf2c4
Minh Khoa <88275892+IAmMinhKhoa@users.noreply.github.com>
2023-12-25 01:01:36 +0700

    2

---
+++ Assets/Main/Scrips/GameMerge/TouchSlider.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine ;
+using UnityEngine.Events ;
+using UnityEngine.EventSystems ;
+using UnityEngine.UI ;
+
+public class TouchSlider : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+   public UnityAction OnPointerDownEvent ;
+   public UnityAction<float> OnPointerDragEvent ;
+   public UnityAction OnPointerUpEvent ;
+
+   private Slider uiSlider ;
+
+   private void Awake () {
+      uiSlider = GetComponent<Slider> () ;
+      uiSlider.onValueChanged.AddListener (OnSliderValueChanged) ;
+   }
+    private void Update()
+    {
+       // Debug.Log(uiSlider.value);
+    }
+    public void OnPointerDown (PointerEventData eventData) {
+      if (OnPointerDownEvent != null)
+         OnPointerDownEvent.Invoke () ;
+
+      if (OnPointerDragEvent != null)
+         OnPointerDragEvent.Invoke (uiSlider.value) ;
+   }
+
+   private void OnSliderValueChanged (float value) {
+        Debug.Log(value);
+      if (OnPointerDragEvent != null)
+         OnPointerDragEvent.Invoke (value) ;
+   }
+
+   public void OnPointerUp (PointerEventData eventData) {
+      if (OnPointerUpEvent != null)
+         OnPointerUpEvent.Invoke () ;
+
+      // reset slider value:
+      uiSlider.value = 0f ;
+   }
+
+   private void OnDestroy () {
+      //remove listeners: to avoid memory leaks
+      uiSlider.onValueChanged.RemoveListener (OnSliderValueChanged) ;
+   }
+}
